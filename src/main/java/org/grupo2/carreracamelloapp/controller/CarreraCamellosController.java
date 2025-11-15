2a9c6d961150203f1bd46261a6324ffd806dfc80
javier <javier@example.com>
2025-11-15 21:22:49 +0100

    Winner Label en UI

--- src/main/java/org/grupo2/carreracamelloapp/controller/CarreraCamellosController.java
+++ src/main/java/org/grupo2/carreracamelloapp/controller/CarreraCamellosController.java
@@ -3,6 +3,7 @@ package org.grupo2.carreracamelloapp.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
 public class CarreraCamellosController {
 
@@ -16,11 +17,19 @@ public class CarreraCamellosController {
     private ImageView camello3;
 
     @FXML
+    private Text winnerLabel;
+
+    @FXML
     protected void onIniciarCarreraClick(ActionEvent event) {
         double moveDistance = 30; // píxeles a mover por clic
         camello1.setLayoutX(camello1.getLayoutX() + moveDistance);
         camello2.setLayoutX(camello2.getLayoutX() + moveDistance);
         camello3.setLayoutX(camello3.getLayoutX() + moveDistance);
+    }
+
+    @FXML
+    protected void mostrarWinner(String ganador) {
+        winnerLabel.setText("El ganador de la carrera es: " + ganador);
     }
 
 }
